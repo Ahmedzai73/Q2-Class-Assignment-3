c64bce828fce4a6db74a48d6926f60db373c468d
Ahmedzai73 <ahmedzai73@gmail.com>
2024-11-08 22:21:21 +0500

    error issue solved

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <div className="containter w-[1920px] h-[92px] flex justify-between items-center py-4 px-[220px] bg-[#043873] font-[inter]">
         <div className="logo w-[191px] h-[34px]"><Link href="/"><Image src={logo} alt="" /></Link></div>
-      <div className=" w-[737.5px] h-[60px] flex">
+      <div className=" w-[737.5px] h-[60px] flex justify-start">
        
         <div className="w-[549px] h-[23px]  my-[18.5] flex items-center justify-start font-medium text-lg gap-[32px] ">
            <Link href="/" className="hover:text-[#dac27c]">Products</Link> 
